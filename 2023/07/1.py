b901bff3e6c03b3c89320ff8e1d76555bf069c07
Logan George <logangeorge01@gmail.com>
2023-12-07 15:31:56 -0800

    start of day 8 using hexadecimal

---
+++ 2023/07/1.py
@@ -0,0 +1,16 @@
+
+
+a = []
+
+with open('input.txt') as f:
+   for l in f:
+      a.append(l.strip().split(' '))
+
+s = [[0, 0]] * len(a)
+
+k = {'K': ''}
+
+for i in range(len(a)):
+    s[i][1] = in
+
+
