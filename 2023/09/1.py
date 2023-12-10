5337b775632618e949d74021fe553e014a216dc6
Logan George <logangeorge01@gmail.com>
2023-12-09 17:52:14 -0800

    day 9 - pt 2 2EZ what are we doing?

---
+++ 2023/09/1.py
@@ -0,0 +1,22 @@
+
+
+a = []
+
+with open('input.txt') as f:
+   for l in f:
+      a.append([int(n) for n in l.split(' ')])
+
+
+def rec(ar):
+   if ar[-2:] == [0, 0]:
+      return 0
+   nar = []
+   for i in range(len(ar)-1):
+      nar.append(ar[i+1] - ar[i])
+   return ar[-1] + rec(nar)
+
+s = 0
+for l in a:
+   s += rec(l)
+
+print(s)
